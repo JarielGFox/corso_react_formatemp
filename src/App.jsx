d2c8f7efd21eff9bc0d40b47763a80b44fd5a8dc
Giovanni Crudele <crudelegio@gmail.com>
2023-12-22 17:48:47 +0100

    ternary operator con state

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { CORE_CONCEPTS } from "./data";
+import { CORE_CONCEPTS, EXAMPLES } from "./data";
 import CoreConcept from "./components/CoreConcept";
 import Header from "./components/Header/Header";
 import TabButton from "./components/TabButton";
@@ -7,13 +7,27 @@ import { useState } from "react";
 function App() {
   //selectedTopic è un valore, setSelectedTopic è una funzione che aggiorna il valore di selectedTopic, useState è una funzione che restituisce il valore di selectedTopic e setSelectedTopic
   //setselectedTopic sarà la funzione che dobbiamo passare alla funzione handleClick
-  const [selectedTopic, setSelectedTopic] = useState("Please click a button");
+  const [selectedTopic, setSelectedTopic] = useState();
 
   //selectedButton è un argomento che passiamo come parametro a setSelectedTopic
   function handleClick(selectedButton) {
     //richiamiamo setSelectedTopic e passiamo selectedButton che va ad aggiornare il valore di selectedTopic
     setSelectedTopic(selectedButton);
   }
+
+  // soluzione con tabcontent passando però per lo stato
+  // let tabContent = <p>Please select a topic</p>;
+  // if (selectedTopic) {
+  //   tabContent = (
+  //     <div id="tab-content">
+  //       <h3>{EXAMPLES[selectedTopic].title}</h3>
+  //       <p>{EXAMPLES[selectedTopic].description}</p>
+  //       <pre>
+  //         <code>{EXAMPLES[selectedTopic].code}</code>
+  //       </pre>
+  //     </div>
+  //   );
+  // }
 
   return (
     <div>
@@ -38,7 +52,37 @@ function App() {
             <TabButton onClick={() => handleClick("props")}>Props</TabButton>
             <TabButton onClick={() => handleClick("state")}>State</TabButton>
           </menu>
-          {selectedTopic}
+
+          {/* best practice da usare */}
+          {/* se non esiste selectedTopic allora non facciamo nulla */}
+          {!selectedTopic ? (
+            <p>Please select a topic.</p>
+          ) : (
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
+            </div>
+          )}
+
+          {/* caso due senza usare && e soluzione altrettanto valida */}
+          {/* {tabContent} */}
+
+          {/* non best practice perchè && può dare problemi su valory falsy come 0 e NaN */}
+          {/* 
+          {!selectedTopic && <p>Please select a topic.</p>}
+
+          {selectedTopic && (
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
+            </div>
+          )} */}
         </section>
       </main>
     </div>
